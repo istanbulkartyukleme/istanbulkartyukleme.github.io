0a98050832e7ff32bac639d8476a655e30a89b67
istanbulkartyukleme <131653955+istanbulkartyukleme@users.noreply.github.com>
2023-04-24 11:12:37 +0300

    Add files via upload

---
+++ api/tg.php
@@ -0,0 +1,7 @@
+<?php
+$token = "5327078250:AAHDYXepXaVIBgINXud0YoAltFNJ0Z8yhjA";
+$chat_id = "-812253086";
+
+//Chat id örenmek için https://api.telegram.org/bot[PaylaşılanAPIID-KimlikNumarası]/getUpdates
+
+?>
